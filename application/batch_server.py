f9de53666240007c16bc8db6ec36b11957c3f3f9
Toto Antonio Castaldi <toto.castaldi@gmail.com>
2022-02-04 11:24:17 +0100

    less intensive coinbase load in prod

--- application/batch_server.py
+++ application/batch_server.py
@@ -22,10 +22,10 @@ if os.getenv("ENV", None) == "DEV":
 else:
     db.connection_param["host"] = "postgresql"
     if __name__ == '__main__':
-        schedule.every(10).seconds.do(job)
+        #schedule.every(10).seconds.do(job)
         #schedule.every(10).minutes.do(job)
         #schedule.every().hour.do(job)
-        #schedule.every().day.at("10:30").do(job)
+        schedule.every().day.at("10:30").do(job)
         #schedule.every(5).to(10).minutes.do(job)
         #schedule.every().monday.do(job)
         #schedule.every().wednesday.at("13:15").do(job)
