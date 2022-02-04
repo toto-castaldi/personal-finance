dd959c1d1396800e41e4fa1feaa88b06a62c2c04
Toto Antonio Castaldi <toto.castaldi@gmail.com>
2022-02-04 12:35:33 +0100

    file logging

--- application/common/utils.py
+++ application/common/utils.py
@@ -10,7 +10,18 @@ LOG_LEVELS = {
 }
 
 log_level =  LOG_LEVELS.get(os.environ.get('LOG_LEVEL','INFO'))
+logger_files = ['batch']
 logger = None
+
+class ProcessnameFilter(logging.Filter):
+  processName = None
+
+  def __init__(self, processName):
+    super().__init__()
+    self.processName = processName 
+
+  def filter(self, record):
+    return True if self.processName in record.processName else False
 
 def init_log():
   global logger
@@ -19,10 +30,20 @@ def init_log():
     streamHandler = logging.StreamHandler()
     streamHandler.setLevel(log_level)
     formatter = logging.Formatter('[%(levelname)s] - %(asctime)s : %(message)s', datefmt='%Y/%m/%d %H:%M:%S')
+    f_formatter = logging.Formatter('[%(levelname)s] - %(asctime)s.%(msecs)03d: %(message)s', datefmt='%Y/%m/%d %H:%M:%S')
     streamHandler.setFormatter(formatter)
 
     logger = logging.getLogger()
     logger.addHandler(streamHandler)
+
+    for lf in logger_files:
+      f_handler = logging.handlers.TimedRotatingFileHandler(f'./log/{lf}.log', when='midnight', backupCount=10)
+      f_handler.setFormatter(f_formatter)
+      f_handler.addFilter(ProcessnameFilter(lf))
+      f_handler.setLevel(log_level)
+      
+      logger.addHandler(f_handler)
+
     logger.setLevel(log_level)
     
   return logger
