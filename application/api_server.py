0d5f279ae88fe8c7dca179fed6d5a4e0d64710e3
Toto Antonio Castaldi <toto.castaldi@gmail.com>
2022-02-08 14:23:16 +0100

    Could not import module "api.routes "

--- application/api_server.py
+++ application/api_server.py
@@ -6,4 +6,4 @@ if __name__ == "__main__":
   if utils.is_dev_env():
     uvicorn.run("api.routes:app", port=5000, reload=True, workers=1)
   else:
-    uvicorn.run("api.routes :app", host='0.0.0.0', port=5000, workers=10)
+    uvicorn.run("api.routes:app", host='0.0.0.0', port=5000, workers=10)
