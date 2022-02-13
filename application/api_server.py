31071669a8e32876d822209804f3656d62ed50fb
Toto Antonio Castaldi <toto.castaldi@gmail.com>
2022-02-13 19:23:26 +0100

    could not connect to server: Cannot assign requested addres

--- application/api_server.py
+++ application/api_server.py
@@ -1,9 +1,11 @@
 import uvicorn
 import sys
 import common.utils as utils
+import common.db as db
 
 if __name__ == "__main__":
   if utils.is_dev_env():
     uvicorn.run("api.routes:app", port=5000, reload=True, workers=1)
   else:
+    db.connection_param["host"] = "postgresql"
     uvicorn.run("api.routes:app", host='0.0.0.0', port=5000, workers=10)
