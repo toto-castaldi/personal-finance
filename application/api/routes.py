cc841528b891340aa76ca4b3f1cab9dfe1b0b0d1
Toto Antonio Castaldi <toto.castaldi@gmail.com>
2022-02-13 22:47:01 +0100

    portfolio csv

--- application/api/routes.py
+++ application/api/routes.py
@@ -2,6 +2,7 @@ from locale import currency
 import common.utils as utils
 import common.db as db
 import common.portfolio as portfolio
+import common.bean as bean
 import json
 from fastapi import Response
 from fastapi import FastAPI
@@ -12,11 +13,28 @@ logger = utils.init_log()
 
 
 @app.get("/portfolio-values/{account_id}/{currency}")
-def portfolio_values(account_id: str, currency: str):
+def portfolio_values_json(account_id: str, currency: str):
     logger.debug(account_id)
     if db.account_info(account_id):
         asset_points = portfolio.Portfolio(account_id).asset_points(native_currency=currency)
         return Response(content=json.dumps(asset_points, default=utils.json_serial), media_type="application/json")
+    else:
+        raise HTTPException(status_code=404, detail="wrong account")
+
+@app.get("/portfolio-values-csv/{account_id}/{currency}")
+def portfolio_values_csv(account_id: str, currency: str):
+    logger.debug(account_id)
+    if db.account_info(account_id):
+        asset_points = portfolio.Portfolio(account_id).asset_points(native_currency=currency)
+        headers = {"Content-Disposition": "attachment;filename=portfolio.csv"}
+        res = "DATE, AMOUNT" 
+        for ap in asset_points:
+            total = 0
+            for a in ap.assets:
+                if isinstance(a, bean.ConvertedAssetAmount):
+                    total += a.native_amount
+            res += "\n" + f"{ap.the_date},{total}"
+        return Response(content=res, media_type="text/csv", headers=headers)
     else:
         raise HTTPException(status_code=404, detail="wrong account")
 
