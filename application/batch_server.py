649b22dc415ea0e40d0407fc01746d5bf5b81d15
Toto Antonio Castaldi <toto.castaldi@gmail.com>
2022-02-22 11:15:04 +0100

    every two hours

--- application/batch_server.py
+++ application/batch_server.py
@@ -13,7 +13,7 @@ else:
     if __name__ == '__main__':
         schedule.every(10).minutes.do(batch.tick_job)
         schedule.every().day.at("00:01").do(batch.coinapi_job)
-        schedule.every().day.at("14:30").do(batch.coinbase_job)
+        schedule.every(2).hours.do(batch.coinbase_job)
         
         #schedule.every().hour.do(tick_job)
         #schedule.every(10).seconds.do(tick_job)
