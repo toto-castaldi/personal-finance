ceff2ec33fd905bf1fd1e2b17b268c6cc75a6dfa
Toto Antonio Castaldi <toto.castaldi@gmail.com>
2022-03-14 19:48:56 +0100

    logout

--- site/src/App.vue
+++ site/src/App.vue
@@ -25,6 +25,9 @@
             <li class="nav-item">
               <router-link class="nav-link active" to="/sign-in">Sign In</router-link>
             </li>
+            <li class="nav-item">
+              <button @click="handleSignOut" v-if="isLoggedIn" >Sign Out</button>
+            </li>
           </ul>
         </div>
       </div>
@@ -35,3 +38,28 @@
     <router-view />
   </main>
 </template>
+
+<script setup>
+  import { onMounted, ref } from "vue";
+  import { getAuth, onAuthStateChanged, signOut } from "firebase/auth";
+  import { useRouter} from "vue-router";
+  const isLoggedIn = ref(false);
+  const router = useRouter();
+  let auth;
+  onMounted(() => {
+    auth = getAuth();
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        isLoggedIn.value = true;
+      } else {
+        isLoggedIn.value = false;
+      }
+    });
+  });
+
+  const handleSignOut = () => {
+    signOut(auth).then(() => {
+      router.push("/");
+    });
+  }
+</script>
--- site/src/views/SignIn.vue
+++ site/src/views/SignIn.vue
@@ -3,7 +3,44 @@
     <div class="row">
       <div class="col-md-12">
           <h1>Sign In</h1>
+          <p><input type="text" v-model="email" placeholder="Email" /></p>
+          <p><input type="password" v-model="password" placeholder="Password" /></p>
+          <p v-if="errorMessage">{{errorMessage}}</p>
+          <p><button @click="register">Submit</button></p>
+          <p><button @click="signInWithGoogle">Sign In with Google</button></p>
       </div>
     </div>
   </div>
 </template>
+<script setup>
+    import { ref } from "vue";
+    import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+    import { useRouter} from "vue-router";
+    const email = ref("");
+    const password = ref("");
+    const errorMessage = ref();
+    const router = useRouter();
+    const register = () => {
+        const auth = getAuth();
+        signInWithEmailAndPassword(auth, email.value, password.value)
+        .then((data) => {
+            console.log("successfully signed in");
+            console.log(auth.currentUser);
+            router.push("/portfolio");
+        })
+        .catch((error) => {
+            console.log(error.code);
+            switch(error.code) {
+                case "auth/invalid-email":
+                    errorMessage.value = "Invalid Email";
+                    break;
+                default:
+                    errorMessage.value = "Invalid Email or Password";
+                    break;
+            }
+        })
+    }
+    const signInWithGoogle = () => {
+
+    }
+</script>
