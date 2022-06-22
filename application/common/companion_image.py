d19248128a23b337cc30427225a2d28c95dc1d84
Antonio Toto Castaldi <toto.castaldi@gmail.com>
2022-06-22 18:35:26 +0200

    error folder

--- application/common/companion_image.py
+++ application/common/companion_image.py
@@ -27,6 +27,7 @@ def job():
         upload_folder = constants.get_config()["upload_folder"]
         worked_folder = constants.get_config()["worked_folder"]
         unknow_folder = constants.get_config()["unknow_folder"]
+        error_folder = constants.get_config()["error_folder"]
         onlyfiles = [f for f in listdir(upload_folder) if isfile(join(upload_folder, f)) and "-type-image" in f]
         
         for f in onlyfiles:
@@ -39,39 +40,48 @@ def job():
 
             footer_last_line = extract_footer_last_line(image, 720/868)
             if "Negozi" in footer_last_line and "Contatti" in footer_last_line and "Servizi" in footer_last_line and "Invita" in footer_last_line and "Profilo" in footer_last_line :
-                image_type = "SATISPAY"
-                logger.info(f"{full_path} is a Satispay SCREENSHOT")
+                try:
+                    image_type = "SATISPAY"
+                    logger.info(f"{full_path} is a Satispay SCREENSHOT")
 
-                disponibilita = image.crop((0, h * (115/868), w / 2, h * (225/868) ))
-                #disponibilita.save("/home/toto/tmp/abce/disponibilita.png")
+                    disponibilita = image.crop((0, h * (115/868), w / 2, h * (225/868) ))
+                    #disponibilita.save("/home/toto/tmp/abce/disponibilita.png")
 
-                risparmi = image.crop((w / 2, h * (115/868), w , h * (225/868) ))
-                #risparmi.save("/home/toto/tmp/abce/risparmi.png")
+                    risparmi = image.crop((w / 2, h * (115/868), w , h * (225/868) ))
+                    #risparmi.save("/home/toto/tmp/abce/risparmi.png")
 
-                disponibilita_lines = pytesseract.image_to_string(disponibilita, config=custom_oem_psm_config).splitlines()
-                risparmi_lines = pytesseract.image_to_string(risparmi, config=custom_oem_psm_config).splitlines()
+                    disponibilita_lines = pytesseract.image_to_string(disponibilita, config=custom_oem_psm_config).splitlines()
+                    risparmi_lines = pytesseract.image_to_string(risparmi, config=custom_oem_psm_config).splitlines()
 
-                disponibilita_euro = Decimal(utils.str_euro_to_number(disponibilita_lines[-1]))
-                risparmi_euro = Decimal(utils.str_euro_to_number(risparmi_lines[-1]))
+                    disponibilita_euro = Decimal(utils.str_euro_to_number(disponibilita_lines[-1]))
+                    risparmi_euro = Decimal(utils.str_euro_to_number(risparmi_lines[-1]))
 
-                logger.debug(f"{disponibilita_euro} {risparmi_euro}")
-                
-                db.save_satispay(account_id, today, disponibilita_euro, risparmi_euro, "EUR", f)
+                    logger.debug(f"{disponibilita_euro} {risparmi_euro}")
+                    
+                    db.save_satispay(account_id, today, disponibilita_euro, risparmi_euro, "EUR", f)
+
+                except:
+                    logger.error("exception ",exc_info=1)
+                    utils.move_file(full_path, error_folder)
 
             footer_last_line = footer_last_line = extract_footer_last_line(image, 760/868)
             if "Mercato" in footer_last_line and "Preferiti" in footer_last_line and "Portafoglio" in footer_last_line and "Attivita" in footer_last_line :
-                image_type = "DEGIRO"
-                logger.info(f"{full_path} is a Degiro SCREENSHOT")
-                bank_amount_image = image.crop((w * (40/400), h * (27/868), w / 2 - w * (6/400), h * (59/868) ))
-                if utils.is_dev_env():
-                    bank_amount_image.save(f"{uuid_file}-bank_amount.png")
+                try:
+                    image_type = "DEGIRO"
+                    logger.info(f"{full_path} is a Degiro SCREENSHOT")
+                    bank_amount_image = image.crop((w * (40/400), h * (27/868), w / 2 - w * (6/400), h * (59/868) ))
+                    if utils.is_dev_env():
+                        bank_amount_image.save(f"{uuid_file}-bank_amount.png")
 
-                amount_tesseract = pytesseract.image_to_string(bank_amount_image, config=custom_oem_psm_config);
+                    amount_tesseract = pytesseract.image_to_string(bank_amount_image, config=custom_oem_psm_config);
 
-                bank_amount = Decimal(utils.str_euro_to_number(amount_tesseract.splitlines()[0]))
-                logger.debug(bank_amount)
+                    bank_amount = Decimal(utils.str_euro_to_number(amount_tesseract.splitlines()[0]))
+                    logger.debug(bank_amount)
 
-                db.save_degiro_balance(account_id, today, bank_amount, "EUR", f)
+                    db.save_degiro_balance(account_id, today, bank_amount, "EUR", f)
+                except:
+                    logger.error("exception ",exc_info=1)
+                    utils.move_file(full_path, error_folder)
 
             if image_type:
                 utils.move_file(full_path, worked_folder)
