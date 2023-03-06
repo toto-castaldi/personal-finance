2ffeff0f81a92e11ad1b136fdbefa34ca75b4aab
Antonio Toto Castaldi <toto.castaldi@gmail.com>
2023-03-06 10:16:21 +0100

    Crypto buiyng format

--- site/src/components/CryptoMovements.vue
+++ site/src/components/CryptoMovements.vue
@@ -3,7 +3,8 @@
     <div class="card-header">Crypto buiyng</div>
     <div class="card-body">
       <div class="table-responsive">
-        <p class="card-text">Total : {{ amount }}</p>
+        <p class="card-text" v-if="amount">Total : {{ $n(amount, 'currency') }}</p>
+        <p class="card-text" v-else>Total : ...</p>
         <table class="table">
           <thead>
             <tr>
@@ -17,7 +18,7 @@
             <tr v-for="m in movements" :key="m.id">
               <th scope="row">{{m.updated_at}}</th>
               <td>{{m.provider}}</td>
-              <td>{{m.native_amount_amount}}</td>
+              <td>{{ $n(m.native_amount_amount, 'currency') }}</td>
               <td>{{m.crypto_amount_currency}} {{m.crypto_amount_amount}}</td>
             </tr>
           </tbody>
@@ -33,7 +34,7 @@ import { getAuth } from "firebase/auth";
 export default {
     data() {
         return {
-          amount : "...",
+          amount : undefined,
           movements : []
         }
     },
@@ -48,14 +49,11 @@ export default {
         );
         const rjson = await response.json();
 
-        const amount = Number(rjson.total_amount);
-        const currency = rjson.total_currency === "EUR" ? "€" : rjson.total_currency;
-        this.amount = `${amount.toFixed(2)} ${currency} `;
+        this.amount = Number(rjson.total_amount);
 
         this.movements = rjson.movements.map(m => {
-          const amount = Number(m.native_amount_amount);
-          const currency = m.native_amount_currency === "EUR" ? "€" : m.native_amount_currency;
-          m.native_amount_amount = `${amount.toFixed(2)} ${currency} `;
+          m.updated_at = new Date(Date.parse(m.updated_at)).toLocaleDateString(navigator.language, { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' });
+          m.native_amount_amount = Number(m.native_amount_amount);
           return m;
         });
       }
